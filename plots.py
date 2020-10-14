00dadeecc8d409690f46930983f8b93c903ab79d
eivskeiv <eivinkoh@stud.ntnu.no>
2020-10-14 14:07:07 +0200

    Can now choose between wavelength or band along xaxis

--- plots.py
+++ plots.py
@@ -7,8 +7,17 @@ import matplotlib.pyplot as plt
 
 
 
-def plotSpectrum(arr):
-    x = np.linspace(400,1000,len(arr))
-    plt.plot(x,arr)
+def plotSpectrum(arr, xaxis="wavelength"):
+
+    if xaxis == "wavelength":
+        x = np.linspace(400,1000,len(arr))
+    elif xaxis == "band":
+        x = np.linspace(0,len(arr), len(arr))
+        
+    else:
+        print("xaxis wasnt wavelength or band")
+        return 0
+    
+    plt.plot(x, arr)
     plt.ylabel("Reflectanse")
-    plt.xlabel("Wavelength")
+    plt.xlabel(xaxis)
